4a4ce945d5c049ec84e72e371517e6a7163e3eb0
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-12-02 16:50:52 +0600

    some fix

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -5,7 +5,7 @@ export const AboutSection = () => {
       <hr className="border-t-2 border-gray-500 lg:w-1/2 md:w-full sm:w-full" />
       <div className="text-center max-w-4xl">
         <p className="text-start font-sans text-gray-400 leading-7">
-          I'm <span className="text-6xl font-extrabold text-white md:text-5xl sm:text-xl">Joyant Sheikhar Gupta Joy</span>, <br /> a <span className="text-sky-400 font-semibold">Software Developer</span> and Computer Science & Engineering student passionate about problem-solving and creating innovative, efficient solutions. <br /> With expertise in frontend and backend development, I strive to blend design and functionality seamlessly.        </p>
+          I&apos;m <span className="text-6xl font-extrabold text-white md:text-5xl sm:text-xl">Joyant Sheikhar Gupta Joy</span>, <br /> a <span className="text-sky-400 font-semibold">Software Developer</span> and Computer Science & Engineering student passionate about problem-solving and creating innovative, efficient solutions. <br /> With expertise in frontend and backend development, I strive to blend design and functionality seamlessly.        </p>
       </div>
     </div>
   );
