c046e20310d960ed1c6cb86a986496d3cfae0440
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-03-21 18:37:46 +0600

    resume change

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -74,7 +74,7 @@ export const Header = () => {
             <li><a href="#project">Projects</a></li>
           </ul>
         </div>
-          <a href="/assets/file/Joyant_resume.pdf" download="Joyant_resume">
+          <a href="/assets/file/Joyant_Resume.pdf" download="Joyant_Resume">
             <button className="hidden group relative lg:inline-flex md:inline-flex items-center justify-center overflow-hidden rounded-full bg-gradient-to-r from-gray-300/75 to-gray-700 text-gray-950 backdrop-blur-lg px-4 py-2 text-base font-semibold border border-white/20" type="button">
                 <span className="">Resume</span>
                 <ArrowDownIcon className="size-5"/>
