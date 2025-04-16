2cd9009d17fcb6e5d646f029403af7da4051fc58
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-04-16 07:05:08 +0600

    mobile navbar updated

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,6 +4,7 @@ import myImage from "@/assets/images/joyant.png"
 import Image from 'next/image'
 import { useState, useEffect } from "react"
 import { Download } from "lucide-react";
+import MobileNavbar from "./MobileNavbar";
 
 const useScrollPosition = (threshold = 50) => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -23,16 +24,11 @@ const useScrollPosition = (threshold = 50) => {
 
 export const Header = () => {
   const isScrolled = useScrollPosition(50);
-
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-
   return (
     <div className={`flex justify-between items-center fixed py-3 px-10 w-full z-10 transition-all duration-300 ${isScrolled ? " backdrop-blur-md" : "bg-transparent"}`}>
-        <div className="flex items-center gap-2">
-          <Image src={myImage} alt="Logo" className="w-16 h-16" />
-        </div>
+      <div className="flex items-center gap-2">
+        <Image src={myImage} alt="Logo" className="w-16 h-16" />
+      </div>
       <div className="ml-16 sm:hidden md:block lg:block">
         <nav className="flex gap-1 p-0.5 font-display border border-white/15 rounded-full">
           <a href="#home" className="nav-item">Home</a>
@@ -40,40 +36,9 @@ export const Header = () => {
           <a href="#project" className="nav-item">Projects</a>
         </nav>
       </div>
-      <div className="flex sm:items-center sm:justify-center">
-        <div className="dropdown relative md:hidden lg:hidden">
-          <div tabIndex={0} role="button" className="btn btn-ghost text-white" onClick={toggleMenu}>
-          {isMenuOpen ? (
-            // Cross Icon
-            <svg className="menu-opener__squarer" width="12" height="12" viewBox="0 0 12 12" fill="none">
-              <rect y="10" width="2" height="2" fill="currentColor"></rect>
-              <rect width="2" height="2" fill="currentColor"></rect>
-              <rect x="5" y="5" width="2" height="2" fill="currentColor"></rect>
-              <rect x="10" y="10" width="2" height="2" fill="currentColor"></rect>
-              <rect x="10" width="2" height="2" fill="currentColor"></rect>
-            </svg>
-          ) : (
-            // Hamburger Icon
-            <svg className="menu-opener__square" width="12" height="12" viewBox="0 0 12 12" fill="none">
-              <rect y="10" width="2" height="2" fill="currentColor"></rect>
-              <rect y="5" width="2" height="2" fill="currentColor"></rect>
-              <rect width="2" height="2" fill="currentColor"></rect>
-              <rect x="5" y="10" width="2" height="2" fill="currentColor"></rect>
-              <rect x="5" y="5" width="2" height="2" fill="currentColor"></rect>
-              <rect x="5" width="2" height="2" fill="currentColor"></rect>
-              <rect x="10" y="10" width="2" height="2" fill="currentColor"></rect>
-              <rect x="10" y="5" width="2" height="2" fill="currentColor"></rect>
-              <rect x="10" width="2" height="2" fill="currentColor"></rect>
-            </svg>
-
-          )}
-          </div>
-          <ul tabIndex={0} className={`menu absolute -right-3 font-display menu-sm dropdown-content bg-slate-800 rounded-lg text-gray-200 z-[1] mt-4 w-40 p-5 shadow  ${isMenuOpen ? "block" : "hidden"}`}>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#project">Projects</a></li>
-          </ul>
-        </div>
+      
+      <div className="flex items-center space-x-2">
+          <MobileNavbar />
           <a href="/assets/file/Joyant_Resume.pdf" download="Joyant_Resume">
             <button className="hidden group relative lg:inline-flex md:inline-flex items-center justify-center overflow-hidden rounded-full bg-gradient-to-r from-gray-300/75 to-gray-700 text-gray-950 backdrop-blur-lg px-4 py-2 text-base font-semibold border border-white/20" type="button">
                 <span className="">Resume</span>
--- src/sections/MobileNavbar.tsx
+++ src/sections/MobileNavbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { Button } from '@/components/ui/button'
-import { Sheet, SheetTrigger } from '@/components/ui/sheet'
-import { MenuIcon } from 'lucide-react'
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from '@/components/ui/sheet'
+import { Download, FolderCog, HomeIcon, MenuIcon, UserIcon } from 'lucide-react'
 import React, { useState } from 'react'
 
 function MobileNavbar() {
@@ -11,9 +11,28 @@ function MobileNavbar() {
         <Sheet open={showMobileMenu} onOpenChange={setShowMobileMenu}>
             <SheetTrigger asChild>
                 <Button variant="ghost" size="icon">
-                    <MenuIcon className='size-5' />
+                    <MenuIcon className='size-8' />
                 </Button>
             </SheetTrigger>
+            <SheetContent side="right" className='bg-slate-900 text-white'>
+                <SheetHeader>
+                    <h2 className='text-lg font-semibold'>Menu</h2>
+                </SheetHeader>
+                <div className='mt-6 flex flex-col space-y-6 text-base font-semibold'>
+                    <a href="#home" onClick={() => setShowMobileMenu(false)} className='flex gap-3'>
+                        <HomeIcon className='size-5'/>Home
+                    </a>
+                    <a href="#about" onClick={() => setShowMobileMenu(false)} className='flex gap-3'>
+                        <UserIcon className='size-5'/>About
+                    </a>
+                    <a href="#project" onClick={() => setShowMobileMenu(false)} className='flex gap-3'>
+                        <FolderCog className='size-5'/>Project
+                    </a>
+                    <a href="/assets/file/Joyant_Resume.pdf" download="Joyant_Resume" className='flex gap-3'>
+                        <Download className='size-5' />Resume
+                    </a>
+                </div>
+            </SheetContent>
         </Sheet>
     </div>
   )
